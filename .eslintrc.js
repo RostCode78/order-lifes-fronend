c0ed4221e67f4e7fe35f53bfb6e51deb5fddd213
RostCode78 <zeroxcode78@gmail.com>
2024-06-25 00:52:12 -0600

    Feat: Focused backgroundcolor

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,9 +3,13 @@ module.exports = {
   extends: '@react-native',
   rules: {
     'prettier/prettier': 0,
-    '@typescript-eslint/no-unused-vars': ['warn', { 'argsIgnorePattern': '^_' }],
+    '@typescript-eslint/no-unused-vars': 0,
     '@typescript-eslint/no-shadow': 0,
     'no-catch-shadow': 0,
     'react-native/no-inline-styles': 0,
+    'react/no-unstable-nested-components': [
+      'off' || 'warn' || 'error',
+      { 'allowAsProps': true || false },
+    ],
   },
 };
--- src/navigator/BottomNavigator.tsx
+++ src/navigator/BottomNavigator.tsx
@@ -1,55 +1,104 @@
 import * as React from 'react';
-import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
 import { Pagina1Screen } from '../screens/dashboard/Pagina1Screen';
 import { Pagina2Screen } from '../screens/dashboard/Pagina2Screen';
 import { Pagina3Screen } from '../screens/dashboard/Pagina3Screen';
+import { colores } from '../themes/globalTheme';
+import { StyleSheet, Text, View } from 'react-native';
 
 const Tareas = require('./../../assets/icons/tareas.svg').default;
 const Premios = require('./../../assets/icons/premios.svg').default;
 const Castigos = require('./../../assets/icons/castigos.svg').default;
 
-const Tab = createMaterialBottomTabNavigator();
+const Tab = createBottomTabNavigator();
 
 const TareasIcon = () => <Tareas width={25} height={25} />;
 const PremiosIcon = () => <Premios width={25} height={25} />;
 const CastigosIcon = () => <Castigos width={25} height={25} />;
 
+const renderTabIcon = (IconComponent: any, label: any) => ({ focused, color }: any) => (
+    <View
+        style={{
+            ...styles.iconContainer,
+            ...(focused && styles.iconContainerFocused),
+        }}
+    >
+        <IconComponent/>
+        { focused &&
+            <Text
+                style={{
+                    color: colores.grey800,
+                    fontSize: 15,
+                    fontWeight: 'bold',
+                    marginLeft: 6,
+                }}
+            >{label}</Text>
+        }
+    </View>
+);
+
 export const BottomNavigator = () => {
     return (
         <Tab.Navigator
             initialRouteName="Tareas"
-            activeColor="#f9f5f4"
-            barStyle={{
-                backgroundColor: '#303030',
+            screenOptions={{
+                headerShown: false,
+                tabBarShowLabel: false,
+                tabBarStyle:{
+                    position: 'absolute',
+                    bottom: 25,
+                    marginHorizontal: 20,
+                    elevation: 0,
+                    backgroundColor: colores.white,
+                    borderRadius: 10,
+                    height: 60,
+                    paddingHorizontal: 12,
+                },
             }}
         >
             <Tab.Screen
                 name="Tareas"
                 options={{
-                    title:'Tareas',
-                    tabBarLabel: 'Tareas',
-                    tabBarIcon: TareasIcon,
+                    tabBarIcon: renderTabIcon(TareasIcon, 'Tareas'),
                 }}
                 component={Pagina1Screen}
             />
             <Tab.Screen
                 name="Premios"
                 options={{
-                    title:'Premios',
-                    tabBarLabel: 'Premios',
-                    tabBarIcon: PremiosIcon,
+                    tabBarIcon: renderTabIcon(PremiosIcon, 'Premios'),
                 }}
                 component={Pagina2Screen}
             />
             <Tab.Screen
                 name="Castigos"
                 options={{
-                    title:'Castigos',
-                    tabBarLabel: 'Castigos',
-                    tabBarIcon: CastigosIcon,
+                    tabBarIcon: renderTabIcon(CastigosIcon, 'Castigos'),
+                }}
+                component={Pagina3Screen}
+            />
+            <Tab.Screen
+                name="Gastos"
+                options={{
+                    tabBarIcon: renderTabIcon(CastigosIcon, 'Castigos'),
                 }}
                 component={Pagina3Screen}
             />
         </Tab.Navigator>
     );
 };
+
+const styles = StyleSheet.create({
+    iconContainer: {
+        width: 'auto',
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
+    iconContainerFocused: {
+        backgroundColor: '#a088f8',
+        paddingHorizontal: 12,
+        paddingVertical: 8,
+        borderRadius: 5,
+    },
+});
